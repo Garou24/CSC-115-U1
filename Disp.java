bc00166d900238802897b6153f3cb44fe690fe4c
Brian Coe <brianjcoe@gmail.com>
2020-02-04 01:06:12 -0500

    intial commit for Disp.java

---
+++ Disp.java
@@ -0,0 +1,24 @@
+//Brian Coe
+//Chapter 2 Programming Assignment #2
+//File Debugged to correct errors and formatted to match assignment example
+//
+// Program used in chapter 2 for debugging
+// Sandy Brown
+// Sept 2018
+//
+
+public class Disp { 
+  
+
+   public static void main(String[] args) { 
+    
+      System.out.println("My Name is Professor Brown\n");
+      
+      System.out.println("10 + 20\n");
+      System.out.println(10+20);
+      System.out.print("\n");
+      System.out.print("10+20 is " + (10+20));
+    
+   }
+  
+}
