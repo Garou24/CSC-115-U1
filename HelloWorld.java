eb260144e0cc0ecaafa0448dd7e495cbf5e27690
Brian Coe <brianjcoe@gmail.com>
2020-02-04 00:36:10 -0500

    Intial commit for Triangles.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 //Brian Coe
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("Hello, Professor Brown!");
-    }
+   public static void main(String[] args) {
+      System.out.print("Hello, Professor Brown!");
+   }
 }
---
+++ Triangles.java
@@ -0,0 +1,16 @@
+//Brian Coe
+//Chapter 2 Programming Assignment #1
+public class Triangles {
+   public static void main(String[] args) {
+      System.out.println("#");
+      System.out.println("##");
+      System.out.println("###");
+      System.out.println("####");
+      System.out.println("#####\n");   
+      System.out.println("  #");
+      System.out.println("  ##");
+      System.out.println("  ###");
+      System.out.println("  ####");
+      System.out.println("  #####\n");
+   }
+}
