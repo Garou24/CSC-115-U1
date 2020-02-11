52347a0f52f0bae35f2fd663b4fe2e456ba9ff60
Brian Coe <brianjcoe@gmail.com>
2020-02-10 23:35:38 -0500

    Updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 //Brian Coe
 public class HelloWorld {
    public static void main(String[] args) {
-      System.out.print("Hello, Professor Brown!");
+      String ai= "J.A.R.V.I.S";
+      System.out.print("\"");
+      System.out.print(ai);
+      System.out.print(", sometimes you gotta run before you can ");
+      System.out.print("walk.\"-Tony Stark\n");
+      System.out.println("\"J.A.R.V.I.S, sometimes you gotta run before you can walk.\"-Tony Stark");
    }
 }
