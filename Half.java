4963a2d434b449f26c69db3816810436f353aacf
Brian Coe <brianjcoe@gmail.com>
2020-02-11 22:32:29 -0500

    intial commit for Half.java from programming assignment 4

---
+++ Half.java
@@ -0,0 +1,33 @@
+//Brian Coe
+//02/10/2020
+//Chapter 2 Programming Assignment #4
+//
+//Defining the class
+public class Half {
+    //Declaring the method
+    public static void main(String[] args) {
+        //Defining a double variable as youName using camelCase and initializing it to 10
+        double youName = 10;
+        //Printing the result for the variable youName divided by 2
+        System.out.println(youName/2);
+        //Printing the result of adding 3 to the variable youName
+        System.out.println(youName+3);
+
+        //Defining an integer variable as newVar using camelCase and initializing it to 20
+        int newVar = 20;
+        //Printing the result of newVar divided by 2
+        System.out.println(newVar/2);
+
+        //Declaring the string variable userName as Brian using camelCase
+        String userName = "Brian";
+        //Displaying the value of the variable userName
+        System.out.println(userName);
+
+        //Declaring the the boolean variable trueFalse using camelCase and initializing it as true
+        boolean trueFalse = true;
+        //Changing the value of trueFalse to false
+        trueFalse = !trueFalse;
+        //Displaying the value of the variable trueFalse
+        System.out.print(trueFalse);
+    }
+}
