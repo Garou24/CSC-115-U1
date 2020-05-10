69083adfb9f51071e189609a440bc5bdb4f0a229
Brian Coe <brianjcoe@gmail.com>
2020-05-10 16:15:07 -0400

    Updated TenNumbers.java to print array in reverse order

--- TenNumbers.java
+++ TenNumbers.java
@@ -1,6 +1,8 @@
 //Brian Coe
 //05/09/2020
 //Week 14 Programming Assignment 1
+//This program will prompt theuser to enter in exactly ten integers which are stored in an array. 
+//Then the program will display the user's input and then display the input in reverse.
 //
 import java.util.Scanner;
 
@@ -12,22 +14,21 @@ public class TenNumbers {
         int[] numbers = new int[10];
 
         System.out.print("Enter a total of 10 integers separated by a space. Hit the Enter key when done. ");
-
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = scanner.nextInt();
         }
         scanner.close();
 
         System.out.print("You entered: ");
-
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + " ");
         }
         System.out.println();
+
         System.out.print("Those numbers in reverse order are: ");
-        for (int i = 9; i >= numbers.length; i--) {
-            System.out.print(numbers[i]);
-        }
+        for (int i = numbers.length-1; i >= 0; i--) {  
+            System.out.print(numbers[i] + " ");  
+        } 
 
             
     }
